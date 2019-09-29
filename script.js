375ec06f095ee0f962177803318e0d3f94cfe42c
Guilherme Coelho <guilhermeismco@gmail.com>
2019-09-29 01:10:08 -0300

    New Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  var x = document.getElementById("mobileNav");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
